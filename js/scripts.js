50b62c8709a862dae9ea9a705d99899211eb146b
Alfonso Gómez <alf2501@hotmail.com>
2022-09-05 15:45:46 -0500

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
-/*!
-* Start Bootstrap - Landing Page v6.0.5 (https://startbootstrap.com/theme/landing-page)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+type='text/javascript'>
+    document.addEventListener("contextmenu", function(event){
+        event.preventDefault();
+    }, false);
+
+document.addEventListener("copy", function(event){
+        event.clipboardData.setData("text/plain", "https://a-gmz25.github.io/Pagina-Consultoria/");
+        event.preventDefault();
+    }, false);
+
+
+
+
+
+    
